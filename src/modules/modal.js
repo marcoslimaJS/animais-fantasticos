760c12723a9537efc845b1be8e091e6a7fa39eb1
Marcos Lima <socram.oluap2003@gmail.com>
2022-12-05 09:27:55 -0300

    refatorando o tooltip, transformando em classe

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,7 +4,7 @@ export default class Modal {
     this.botaoFechar = document.querySelector(btnFechar);
     this.containerModal = document.querySelector(container);
 
-    /* bind this ao callback para fazer refencia ao objeto da classe
+    /* bind this ao callback para fazer referencia ao objeto da classe
     */
     this.eventToggleModal = this.eventToggleModal.bind(this);
     this.cliqueForaModal = this.cliqueForaModal.bind(this);
