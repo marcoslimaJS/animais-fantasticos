fd955d2c5f3021c1badc1656ec1cc930b5c3c36c
Marcos Lima <socram.oluap2003@gmail.com>
2022-12-05 11:47:28 -0300

    refatorando fetch-bitcoin

--- src/modules/fetch-bitcoin.js
+++ src/modules/fetch-bitcoin.js
@@ -1,9 +1,8 @@
-export default function initFetchBitcoin() {
-  const url = 'https://blockchain.info/ticker';
+export default function fetchBitcoin(url, target) {
   fetch(url)
     .then((res) => res.json())
     .then(({ BRL: { buy } }) => {
-      const bitcoinPreco = document.querySelector('.bitcoin-preco');
+      const bitcoinPreco = document.querySelector(target);
       bitcoinPreco.innerText = (1000 / buy).toFixed(4);
     })
     .catch((erro) => console.log(Error(erro)));
